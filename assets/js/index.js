9b37aa7043c5e96ce22082b58c916b941e1d1b9e
gyurison7 <orangetree401@gmail.com>
2024-02-09 01:49:14 +0900

    feat: 전체 재작업

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,243 +12,120 @@ gsap.ticker.add((time) => {
 
 gsap.ticker.lagSmoothing(0);
 
-// header
-$(".menu-btn").hover(function () {
-  $(".menu-btn span").toggleClass("on");
+// aside
+ScrollTrigger.create({
+  trigger: ".whole-wrapper",
+  start: "0% 0%",
+  end: "100% 100%",
+  // markers: true,
+  onUpdate: function (self) {
+    if (self.direction < 1) {
+      $(".scroll-direction").text("SCROLL UP");
+    } else {
+      $(".scroll-direction").text("SCROLL DOWN");
+    }
+  },
 });
 
-$(".menu-btn").click(function () {
-  if ($(this).hasClass("open")) {
-    // 닫기
-    gsap.to(".anchor-menu a", {
-      yPercent: -100,
-      onComplete: () => {
-        $(".menu-btn, .anchor-menu").removeClass("open");
-      },
-    });
-  } else {
-    // 열기
-    $(".menu-btn, .anchor-menu").addClass("open");
-    gsap.fromTo(
-      ".anchor-menu a",
-      {
-        yPercent: -100,
-      },
-      {
-        yPercent: 0,
-        stagger: 0.1,
-      }
-    );
-  }
-});
-
-// anchor-menu
 // 스크롤 퍼센트 계산하기
-// lenis.on("scroll", (e) => {
-//   // 전체 문서 길이에서 뷰포트 높이 빼기
-//   const totalScrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-//   const currentScroll = e.targetScroll; // 현재 스크롤 위치
-//   const scrollPercentage = (currentScroll / totalScrollHeight) * 100;
-// });
+lenis.on("scroll", (e) => {
+  // 전체 문서 길이에서 뷰포트 높이 빼기
+  const totalScroll = document.documentElement.scrollHeight - window.innerHeight;
+  const currentScroll = e.targetScroll; // 현재 스크롤 위치
+  const scrollPercentage = (currentScroll / totalScroll) * 100;
+  $(".scroll-bar").css("--progress", `${scrollPercentage.toFixed()}%`);
+});
 
-$(".anchor-menu a").click(function (e) {
+$(".gnb a").click(function (e) {
   e.preventDefault();
   const target = $(this).attr("href");
   const element = $(target);
+  const targetOffset = element.offset().top;
+  lenis.scrollTo(targetOffset);
+});
 
-  if (target === "#home") {
-    lenis.scrollTo(0);
-  } else if (target === "#introduce") {
-    const totalScrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-    // 퍼센테이지를 다시 픽셀 값으로 계산
-    scrollPixel = (totalScrollHeight * 11.6) / 100;
-    lenis.scrollTo(scrollPixel);
+// section-title
+document.querySelectorAll(".section-title").forEach(function (element) {
+  const chars = element.querySelectorAll(".char");
+  gsap.from(chars, {
+    scrollTrigger: {
+      trigger: element,
+      start: "0% 50%",
+      end: "100% 50%",
+      scrub: true,
+      // markers: true,
+    },
+    opacity: 0,
+    yPercent: 100,
+    stagger: 0.1,
+  });
+});
+
+// sc-project
+document.querySelectorAll(".content-wrapper").forEach(function (element) {
+  const target = element.closest(".project");
+  gsap.to(element, {
+    scrollTrigger: {
+      trigger: target,
+      start: "0% 0%",
+      end: "100% 0%",
+      // markers: true,
+      toggleClass: { targets: element, className: "on" },
+    },
+  });
+});
+
+const linkText = new SplitType(".content-wrapper .see-project span", { types: "chars" });
+const titleText = new SplitType(".content-wrapper .title", { types: "chars" });
+const contentText = new SplitType(
+  ".content-wrapper .description1, .content-wrapper .description2 li",
+  { types: "chars" }
+);
+
+document.querySelectorAll(".content-wrapper").forEach(function (element) {
+  const chars = element.querySelectorAll(".char");
+  const textMotion = gsap.timeline({
+    scrollTrigger: {
+      trigger: element,
+      start: "0% 50%",
+      end: "100% 0%",
+      scrub: true,
+      // markers: true,
+    },
+  });
+  textMotion.from(chars, { opacity: 0, yPercent: 100, stagger: 0.1 });
+});
+
+$(".see-project, .code-review").hover(function () {
+  const className = $(this).attr("class").replace("-", " ");
+  const cursor = $(".cursor");
+  const dot = $(".cursor-dot");
+  $(cursor).toggleClass("link");
+  if ($(cursor).hasClass("link")) {
+    $(dot).text(className);
   } else {
-    const targetOffset = element.offset().top;
-    lenis.scrollTo(targetOffset);
+    $(dot).text("");
   }
 });
 
-// scroll-indicator
-$(".scroll-indicator a").click(function (e) {
-  e.preventDefault();
-  lenis.scrollTo(0);
+$(".sc-project .title").hover(function () {
+  $(this).closest(".project").find(".dim").toggleClass("on");
 });
 
-// sc-main
-gsap.from(".sc-main .line .char", {
-  y: "21rem",
-  stagger: 0.02,
-  duration: 1,
-});
-
-// gsap.to(".sc-main .line.move", {
-//   scrollTrigger: {
-//     trigger: ".sc-introduce",
-//     start: "0% 100%",
-//     end: "100% 100%",
-//     scrub: true,
-//     // markers: true,
-//   },
-//   xPercent: 7,
-// });
-
-// sc-introduce
-gsap.to(".sc-introduce", {
+// sc-about
+gsap.to(".sc-about p .char", {
   scrollTrigger: {
-    trigger: ".sticky-wrapper",
+    trigger: ".sc-about p",
     start: "0% 0%",
-    end: "50% 0%",
-    scrub: true,
-    invalidateOnRefresh: true,
-    // markers: true,
-    onEnter: function () {
-      $(".sc-introduce, .sc-banner").addClass("light");
-      $(".menu-btn, .scroll-indicator").addClass("toggle");
-    },
-    onLeaveBack: function () {
-      $(".menu-btn, .scroll-indicator").removeClass("toggle");
-    },
-  },
-  xPercent: -100,
-});
-
-gsap.to(".logo-area", {
-  scrollTrigger: {
-    trigger: ".sc-introduce",
-    start: "70% 0%",
-    end: "200% 0%",
-    toggleClass: { targets: ".logo-area", className: "opacity" },
-    // markers: true,
-  },
-});
-
-gsap.to(".sc-introduce p .word", {
-  scrollTrigger: {
-    trigger: ".sc-introduce p",
-    start: "100% 0%",
-    end: "200% 0%",
-    scrub: true,
-    toggleClass: { targets: ".infinite-text span", className: "opacity" },
-    // markers: true,
-  },
-  stagger: 0.2,
-  opacity: 1,
-});
-
-// sc-banner
-const bannerMotion = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".sc-banner",
-    start: "0% 10%",
     end: "100% 0%",
     scrub: true,
     // markers: true,
-    onEnter: function () {
-      $(".sc-introduce, .sc-banner").removeClass("light");
-      $(".menu-btn, .scroll-indicator").removeClass("toggle");
-    },
-    onLeaveBack: function () {
-      $(".sc-introduce, .sc-banner").addClass("light");
-      $(".menu-btn, .scroll-indicator").addClass("toggle");
-    },
   },
-});
-bannerMotion
-  .to(".sc-banner svg path", { opacity: 1 }, "a")
-  .to(".sc-banner .title", { opacity: 1 }, "a");
-for (let i = 1; i <= 8; i++) {
-  bannerMotion.from(
-    `.sc-banner .word${i}`,
-    { transform: `translateX(${window.innerWidth * i}px) rotate(${25 * i}deg)` },
-    "a+=0.1"
-  );
-}
-bannerMotion
-  .to(
-    ".sc-banner svg",
-    {
-      rotate: -180,
-      skewX: 0,
-      skewY: 0,
-      ease: "power2.in",
-    },
-    "a+=0.1"
-  )
-  .to(".sc-banner svg", { scale: 4 }, "b")
-  .to(".sc-banner .title", { scale: 2 }, "b");
-for (let i = 8; i > 0; i--) {
-  bannerMotion.to(
-    `.sc-banner .word${9 - i}`,
-    {
-      transform: `translateX(-${window.innerWidth * i}px) rotate(${25 * i}deg)`,
-      ease: "power2.in",
-    },
-    "c"
-  );
-}
-
-// sc-project
-$(".see-project, .code-review").hover(function () {
-  $(this).closest(".project").find(".img-wrapper").toggleClass("on");
-  $(this).closest(".project").find(".video-wrapper").toggleClass("on");
-});
-
-const skillsMotion = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".sc-skills",
-    start: "0% 100%",
-    end: "50% 0%",
-    scrub: true,
-    // markers: true,
-    onEnter: function () {
-      $(".sc-skills").addClass("light");
-      $(".scroll-indicator").addClass("toggle");
-    },
-    onLeaveBack: function () {
-      $(".scroll-indicator").removeClass("toggle");
-    },
+  opacity: 1,
+  stagger: {
+    from: "random",
+    each: 0.1,
   },
-});
-
-// sc-skills
-const cardMotion = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".sc-skills",
-    start: "0% 20%",
-    end: "50% 0%",
-    scrub: 3,
-    // markers: true,
-    onEnter: function () {
-      $(".menu-btn, .logo-area").addClass("toggle");
-    },
-    onLeave: function () {
-      $(".sc-skills").removeClass("light");
-      $(".menu-btn, .logo-area, .scroll-indicator").removeClass("toggle");
-    },
-    onEnterBack: function () {
-      $(".sc-skills").addClass("light");
-      $(".menu-btn, .logo-area, .scroll-indicator").addClass("toggle");
-    },
-    onLeaveBack: function () {
-      $(".menu-btn, .logo-area").removeClass("toggle");
-    },
-  },
-  ease: "linear",
-});
-const cards = [
-  { selector: ".card1", xValue: window.innerWidth * 1.038, rotation: -16 },
-  { selector: ".card2", xValue: window.innerWidth * 1.045, rotation: 19 },
-  { selector: ".card3", xValue: window.innerWidth * 1.041, rotation: -11 },
-  { selector: ".card4", xValue: window.innerWidth * 1.17, rotation: 19 },
-];
-cards.forEach((card) => {
-  cardMotion.fromTo(
-    card.selector,
-    { transform: `translateX(${card.xValue}px) rotate(${card.rotation}deg)` },
-    { transform: `translateX(-${card.xValue}px) rotate(${card.rotation * -1}deg)` },
-    "a"
-  );
 });
 
 // cursor
@@ -267,6 +144,8 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("mousemove", function (e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
+
+    $(".cursor-position").text(`[x:${mouseX}] [y:${mouseY}]`);
   });
 
   function animate() {
